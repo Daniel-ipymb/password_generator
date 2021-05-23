eabda9d1a9c58298c09d64c95d1a35564cb692ee
Daniel O <danielogunlana23@gmail.com>
2021-05-23 15:36:29 -0400

    added function

--- script.js
+++ script.js
@@ -4,9 +4,24 @@ var generateBtn = document.querySelector("#generate");
 var genNumber = ['0', '1', '2','3','4','5','6','7','8','9']
 var genUpperCase = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')]
 var genLowerCase = ['abcdefghijklmnopqrstuvwxyz'.split('')]
-var genSymbols = ["!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"]
+var genSymbols = [" !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ ".split("")]
+
+
 function generatePassword() {
-  
+  var userInputs = {
+   characterlength: window.prompt("Length of password?"),
+   num: window.prompt("numbers?"),
+   upp: window.prompt("Uppercase?"),
+   low: window.prompt("Lowercase?"),
+   symbol: window.prompt("Special Characters?")
+  }
+  if (num) {
+    
+  }
+  var userChoice = {
+    
+  }
+
 }
 // Write password to the #password input
 function writePassword() {
