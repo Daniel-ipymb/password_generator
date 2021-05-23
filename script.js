99a65a45b39f173783cf452798109d0c5f3ca35d
Daniel O <danielogunlana23@gmail.com>
2021-05-23 11:28:45 -0400

    init commit fixing bug

---
+++ script.js
@@ -0,0 +1,21 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+var genNumber = ['0', '1', '2','3','4','5','6','7','8','9']
+var genUpperCase = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')]
+var genLowerCase = ['abcdefghijklmnopqrstuvwxyz'.split('')]
+var genSymbols = ["!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"]
+function generatePassword() {
+  
+}
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
